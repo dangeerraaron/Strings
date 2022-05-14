3dad355f40afa7b9c862f68965257800881e00c1
Aaron J Lanning <64545232+dangeerraaron@users.noreply.github.com>
2022-05-14 12:43:32 -0400

    Add files via upload
    Program to find length of string.

---
+++ FindLengthofString.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// find length of string
+int main(void){
+   char name[]="welcome";
+   int i=0;
+   
+   for(i=0; name[i]!='\0'; i++) // loop will iterate until null character found
+   {
+   }
+   printf("Length of string is %d.", i);
+    
+    return 0;
+}
